7ba97eac920c49240d977897c08ca5eb7aa75689
denis <dsenkin@amcbridge.com>
2019-01-27 18:58:50 +0200

    w

--- srgan/srgan.py
+++ srgan/srgan.py
@@ -376,7 +376,7 @@ class SRGAN():
 
             imgs_hr_pred = self.discriminator.predict(imgs_hr)
 
-            print(imgs_hr_pred)
+            print(imgs_hr_pred.shape)
             # Train the generators
             g_loss = self.combined.train_on_batch([imgs_lr, imgs_hr], [valid, image_features, imgs_hr_pred, imgs_hr])
             lrate_callback.on_epoch_end(epoch + 1)
