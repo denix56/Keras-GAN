2147cdf9093f179e801e3c4c806e3e30ebb1e64a
denis <dsenkin@amcbridge.com>
2019-02-02 19:12:37 +0200

    w

--- srgan/srgan.py
+++ srgan/srgan.py
@@ -340,7 +340,7 @@ class SRGAN():
                 result[l[0]] = l[1]
             return result
 
-        tb_callback = TensorBoard(batch_size=batch_size, write_grads=False, write_images=True, write_graph=True, histogram_freq=1)
+        tb_callback = TensorBoard(batch_size=batch_size, write_grads=False, write_images=True, write_graph=True)
         tb_callback.set_model(self.combined)
 
         checkpoint_cb = ModelCheckpoint('./checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5', save_best_only=True, period=50)
